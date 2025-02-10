f7b90ef9a991ee7d64dfb59703db09366fa01e58
Aaron Joshua Emmanuel <ajemmanuel45@outlook.com>
2025-02-10 15:21:49 +0530

    Fix render port binding

--- WonderSri_backend/settings.py
+++ WonderSri_backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-5!$46%z#h9+ni4c!i77w-x=@5+(q2xhe8cuz_if+r__v0$wa7p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://wondersri-backend.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
--- manage.py
+++ manage.py
@@ -15,7 +15,8 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
+    port = os.getenv("PORT", "8000")  # Use Render's PORT or default to 8000
+    execute_from_command_line(["manage.py", "runserver", f"0.0.0.0:{port}"])
 
 
 if __name__ == '__main__':
