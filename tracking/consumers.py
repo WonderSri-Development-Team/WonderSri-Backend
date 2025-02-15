427a41bd6e3fc870426461d25e76243da933beb8
kalpa Perera <klperera660@gmail.com>
2025-02-15 16:49:59 +0530

    simple Web Socket request handlings

--- tracking/consumers.py
+++ tracking/consumers.py
@@ -4,10 +4,36 @@ import json
 class locationConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
-        pass
+        await self.accept()
+        await self.send(json.dumps(
+            {
+                'type' : 'connection',
+                'status' : "connection Accepted"
+            }
+        ))
 
     async def receive(self, text_data):
-        pass
+        print(text_data)
+        data = json.loads(text_data)
+        if data.get('type') == 'location':
+            latitude = data.get('latitude')
+            longitude = data.get('longitude')
+            await self.send(json.dumps({
+            'type': 'location',
+            'latitude': latitude,
+            'longitude': longitude
+            #Location.objects.create(latitude=latitude, longitude=longitude)
+            }))
+        elif data.get('type') == 'connection':
+            await self.send(json.dumps({
+                'type': data.get('type'),
+                'status': data.get('status')
+            }))
     
     async def disconnect(self, code):
-        pass
+        await self.send(json.dumps(
+            {
+                'type' : 'connection',
+                'status' : "connection closed"
+            }
+        ))
