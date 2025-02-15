e82b6854b3022ba7134c3600a4f2a0ae34c633b2
kalpa Perera <klperera660@gmail.com>
2025-02-15 17:12:02 +0530

    setup for corsheaders

--- WonderSri_backend/settings.py
+++ WonderSri_backend/settings.py
@@ -38,10 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'channels',
+    'corsheaders', # for cross origin requests
+
     'tracking',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', # for cross origin requests
+    'django.middleware.common.CommonMiddleware', 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,6 +53,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000", # for local host flutter frontend
 ]
 
 ROOT_URLCONF = 'WonderSri_backend.urls'
