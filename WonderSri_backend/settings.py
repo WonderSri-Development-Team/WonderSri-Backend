fb75a1cb6518a81b6578d776691e63596ecffe62
Aaron Joshua Emmanuel <ajemmanuel45@outlook.com>
2025-03-12 03:05:19 +0530

    Add docker file

--- WonderSri_backend/settings.py
+++ WonderSri_backend/settings.py
@@ -30,11 +30,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["wondersri-backend.onrender.com", "localhost", "127.0.0.1"]
 
-GDAL_LIBRARY_PATH = r"C:\OSGeo4W\bin\gdal310.dll"
 
+# Ensure GDAL paths are set correctly
+# GDAL_LIBRARY_PATH = r"C:\OSGeo4W\bin\gdal310.dll"
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
