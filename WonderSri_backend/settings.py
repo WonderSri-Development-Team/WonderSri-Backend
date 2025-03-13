a70efead303c9b95bcf226df45ef5b69ccdba1db
Aaron Joshua Emmanuel <ajemmanuel45@outlook.com>
2025-03-14 01:21:10 +0530

    Fix Gdal path in deploying on render

--- WonderSri_backend/settings.py
+++ WonderSri_backend/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ["wondersri-backend.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Ensure GDAL paths are set correctly
-GDAL_LIBRARY_PATH = r"C:\OSGeo4W\bin\gdal310.dll"
+# GDAL_LIBRARY_PATH = r"C:\OSGeo4W\bin\gdal310.dll"
 
 # Application definition
 INSTALLED_APPS = [
