1f872d4d40288045d497a961f06900737fc78364
kalpa Perera <klperera660@gmail.com>
2025-03-17 17:07:00 +0530

    adding image_url field to DB and its migrations

--- tracking/models.py
+++ tracking/models.py
@@ -4,9 +4,11 @@ class MainGeofence(models.Model):
     name = models.CharField(max_length=200)
     location = models.PolygonField(geography=True, srid=4326) 
     description = models.TextField(blank=True, null=True)
+    image_url = models.URLField(blank=True, null=True)
 
 class SubGeofence(models.Model):
     name = models.CharField(max_length=255)
     main_geofence = models.ForeignKey(MainGeofence, on_delete=models.CASCADE, related_name='sub_geofences')
     location = models.PolygonField(geography=True, srid=4326)
     description = models.TextField(blank=True, null=True)
+    image_url = models.URLField(blank=True, null=True)
