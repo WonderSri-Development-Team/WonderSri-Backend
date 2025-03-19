cf4811450de7d9fcf9c92dbdd12f788366244438
Aaron Joshua Emmanuel <ajemmanuel45@outlook.com>
2025-03-19 22:43:29 +0530

    Fix allowed host

--- WonderSri_backend/settings.py
+++ WonderSri_backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["wondersri-backend-1.onrender.com", "localhost", "127.0.0.1","wondersri-backend-test.onrender.com"]
+ALLOWED_HOSTS = ["wondersri-backend-1.onrender.com", "localhost", "127.0.0.1","wondersri-backend-test.onrender.com","wondersri-backend.onrender.com"]
 
 
 # Ensure GDAL paths are set correctly
