5d3a00e8c589e38dc07ac8e22e2439196f35b2e8
Chamath Jayashanka <160167403+chamathjay@users.noreply.github.com>
2025-03-22 11:28:31 +0530

    fix location URLs in urlpatterns

--- WonderSri_backend/urls.py
+++ WonderSri_backend/urls.py
@@ -5,7 +5,7 @@ from notifications.views import SendNotificationView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('users.urls')),  # Importing user auth routes
-    # path('api/', include('locations.urls')),
+    path('api/', include('location.urls')),
     path('notifications/', include('notifications.urls')),
     path('send-notification/', SendNotificationView.as_view(), name='send_notification'),
     path('events/', include('events.urls')),
