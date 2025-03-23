0516362a8ef022ae11284340e8d0d61c33b00c4a
Aaron Joshua Emmanuel <ajemmanuel45@outlook.com>
2025-03-23 18:15:21 +0530

    Delete pics & improve logic

--- users/models.py
+++ users/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_picture = models.ImageField(upload_to='profile_pic', null=True, blank=True)
+    profile_picture = models.ImageField( null=True, blank=True)
     dob = models.DateField(null=True, blank=True)
     phone_number = models.CharField(max_length=20, null=True, blank=True)
     pic_url = models.URLField(null=True, blank=True)
